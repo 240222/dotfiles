858e1948c2e8743c58fe298604751a17b4cba77a
Alexander Kleydints <alex@kleydints.com>
2021-12-29 12:29:11 +0100

    Optimised the config.lua.

--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -1,5 +1,28 @@
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
+local options = {
+  expandtab = true,
+  shiftwidth = 2,
+  softtabstop = 2,
+  fileencoding = 'utf-8',
+  hlsearch = true,
+  ignorecase = true,
+  pumheight = 10,
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  undofile = true,
+  cursorline = true,
+  number = true,
+  relativenumber = true,
+  numberwidth = 4,
+  signcolumn = 'yes',
+  scrolloff = 8,
+  termguicolors = true,
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
 vim.cmd [[ colorscheme molokai ]]
