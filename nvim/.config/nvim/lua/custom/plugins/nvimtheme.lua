2b98a54ef32bf85495f99128386a234dcd76f86d
Alex K <kleydints@petalmail.com>
2023-02-23 19:01:51 +0100

    added nvimtheme with support for light and dark themes

---
+++ nvim/.config/nvim/lua/custom/plugins/nvimtheme.lua
@@ -0,0 +1,10 @@
+return {
+  'projekt0n/github-nvim-theme',
+  tag = 'v0.0.7',
+  config = function()
+    require('github-theme').setup({
+      -- theme_style = 'light',
+      theme_style = 'dark',
+    })
+  end
+}
