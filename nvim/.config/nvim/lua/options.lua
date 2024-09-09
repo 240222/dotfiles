124e7989b899bacd797da8a1f52f04f094c28fee
Aleksandr Kleidinc <kleydints@petalmail.com>
2024-09-09 10:53:59 +0300

    added format on save autocommand to options for conform

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,22 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
+
+require("conform").setup({
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
+})
+
