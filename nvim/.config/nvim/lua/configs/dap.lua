19f47280fc800ee850be700863a12fe5a7348e76
Aleksandr Kleidinc <kleydints@petalmail.com>
2024-09-10 18:10:38 +0300

    open dapui windows immediately

--- nvim/.config/nvim/lua/configs/dap.lua
+++ nvim/.config/nvim/lua/configs/dap.lua
@@ -28,3 +28,18 @@ dap.configurations.cpp = {
 
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
+
+-- automatically open dapui windows
+local dapui = require "dapui"
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
